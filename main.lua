fddfd83a7602770ba62f7f5c1573dae999fa89f4
moonconkawin-hub <moonconkawin@gmail.com>
2026-05-25 18:02:20 +0700

    Taho hub

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
+-- Anti Tamper
+local SCRIPT_NAME = "TAHO HUB"
 
+if SCRIPT_NAME ~= "TAHO HUB" then
+    warn("Tamper Detected")
+    return
+end
+
+-- Anti Debug
+local start = tick()
+task.wait(0.1)
+
+if tick() - start > 2 then
+    warn("Debugger detected")
+    return
+end
+
+print("Taho Hub Loaded")
